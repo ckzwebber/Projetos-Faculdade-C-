357d4afd2c9c615fb9314fbbefc43ed2faea60c9
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-04-19 20:39:12 -0300

    Fix

--- Calculadora em Terminal/calculadora.c
+++ Calculadora em Terminal/calculadora.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(void)
+{
 
     int operacao, n1, n2, resultado;
 
-    printf("Escolha a operacao a ser realizada: \n");
-    printf("1 - Adicao \n");
+    printf("Menu de Opcoes: \n");
+    printf("1 - Soma \n");
     printf("2 - Subtracao \n");
     printf("3 - Multiplicacao \n");
     printf("4 - Divisao \n");
-
+    printf("Digite a opcao desejada: ");
     scanf("%d", &operacao);
     printf("\n");
 
-    if (operacao < 1 || operacao > 4) {
+    if (operacao < 1 || operacao > 4)
+    {
         printf("Opcao inexistente!");
         return 1;
     }
@@ -27,23 +29,26 @@ int main(void) {
     scanf("%d", &n2);
     printf("\n");
 
-    if (operacao == 1) {
+    switch (operacao)
+    {
+    case 1:
         resultado = n1 + n2;
-    } else {
-        if (operacao == 2) {
-            resultado = n1 - n2;
-        } else {
-            if (operacao == 3) {
-                resultado = n1 * n2;
-            } else {
-                if (operacao == 4) {
-                    resultado = n1 / n2;
-                }
-            }
-        }
+        break;
+
+    case 2:
+        resultado = n1 - n2;
+        break;
+
+    case 3:
+        resultado = n1 * n2;
+        break;
+
+    case 4:
+        resultado = n1 / n2;
+        break;
     }
 
     printf("O resultado e: %d. \n", resultado);
-    system ("pause");
+    system("pause");
     return 0;
 }
